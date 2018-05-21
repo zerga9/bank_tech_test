a4e31a3867474e01448367959edade3fffa5f4b9
Gadiza Zerari <gadizaz@hotmail.com>
2018-05-21 21:05:39 +0100

    Test statement passing, printing all the transactions

--- spec/statement_spec.rb
+++ spec/statement_spec.rb
@@ -1,6 +1,16 @@
 require 'statement'
 
 describe Statement do
+
+  it 'should print out all transactions' do
+    transactions = [{ date: '16/04/2018', type: 'debit', value: 300, balance: 200 },
+                    { date: '15/04/2018', type: 'credit', value: 500, balance: 500 }]
+    statement = Statement.new(transactions)
+    expect(statement.print_statement).to include({ date: '16/04/2018', type: 'debit', value: 300, balance: 200 },
+                                            {date: '15/04/2018', type: 'credit', value: 500, balance: 500})
+
+
+  end
   it 'should print out all the transactions, last one first' do
     transactions = [{ date: '16/04/2018', type: 'debit', value: 300, balance: 200 },
                     { date: '15/04/2018', type: 'credit', value: 500, balance: 500 }]
