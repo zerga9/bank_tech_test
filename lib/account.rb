da1dc31e6b8dcacdf60be4dcea96bef2ab82842c
Gadiza Zerari <gadizaz@hotmail.com>
2018-05-21 11:34:53 +0100

    test passing gives date of deposit

--- lib/account.rb
+++ lib/account.rb
@@ -1,16 +1,19 @@
 class Account
 
   attr_reader :balance
+  attr_reader :transactions
 
   def initialize
     @balance = 0
+    @transactions = []
   end
 
-  def deposit(value)
+  def deposit(value, date)
     @balance += value
+    @transactions << {date: date, value: value}
   end
 
-  def withdrawal(value)
+  def withdrawal(value, date)
     @balance -= value
   end
 end
--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -2,20 +2,27 @@ require 'account'
 
 describe Account do
 
+  let(:date){ double :date }
+
   it "should have a starting balance of 0" do
     expect(subject.balance).to eq 0
   end
 
   describe '#deposit' do
     it "should add the deposit to your account balance " do
-      expect{ subject.deposit(100) }.to change{ subject.balance }.by 100
+      expect{ subject.deposit(100, date) }.to change{ subject.balance }.by 100
     end
   end
 
   describe '#withdrawal' do
     it "should subtract amount from your account balance " do
-      expect{ subject.withdrawal(50) }.to change{ subject.balance }.by(-50) 
+      expect{ subject.withdrawal(50, date) }.to change{ subject.balance }.by(-50)
     end
+  end
+
+  it 'should give the date of a deposit' do
+    subject.deposit(20, date)
+    expect(subject.transactions[0]).to include date: date
   end
 
 end
