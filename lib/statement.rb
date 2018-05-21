6ef8df1ebba70dc5b7d067f629d9da8a2bcf462c
Gadiza Zerari <gadizaz@hotmail.com>
2018-05-21 12:12:45 +0100

    prints statement as a table

--- lib/statement.rb
+++ lib/statement.rb
@@ -6,8 +6,20 @@ class Statement
     @transactions = []
   end
 
+  def print_header
+    puts "date       || credit  || debit    || balance "
+    puts "---------------------------------------------"
+  end
+
   def print_statement
-    @transactions
+    print_header
+    @transactions.each do |transaction|
+      if transaction[:type] == 'credit'
+        puts "#{transaction[:date]} || #{transaction[:value]} ||  ||  #{transaction[:balance]}"
+      else
+        puts "#{transaction[:date]} ||  || #{transaction[:value]} ||  #{transaction[:balance]}"
+      end
+    end
   end
 
 end
