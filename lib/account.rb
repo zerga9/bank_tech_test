a37f506828ac5dc72fd7604273201b2293cb9d62
Gadiza Zerari <gadizaz@hotmail.com>
2018-05-22 16:30:11 +0100

    Made new account class with new tests for single responsibility principle

--- lib/account.rb
+++ lib/account.rb
@@ -1,20 +1,21 @@
 require_relative 'statement'
 
+
+
 class Account
   attr_reader :balance
-  attr_reader :transactions
+
 
   def initialize
     @balance = 0
-    @transactions = []
   end
 
-  def transaction(value, type, date)
-    type == 'debit' ? @balance -= value : @balance += value
-    transactions << { date: date, type: type, value: value, balance: @balance }
+  def deposit(value)
+    @balance += value
   end
 
-  def print
-    Statement.new(@transactions).print_statement
+  def withdrawal(value)
+    @balance -= value
   end
+
 end
--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -1,34 +1,20 @@
 require 'account'
 
 describe Account do
-  describe '#transaction' do
-    it 'should add the deposit to your account balance ' do
-      expect { subject.transaction(100, 'credit', '15/02/2015') }
-        .to change { subject.balance }.by 100
-    end
 
-    it 'should subtract amount from your account balance ' do
-      expect { subject.transaction(50, 'debit', '13/03/2014') }
-        .to change { subject.balance }.by(-50)
+  describe '#deposit' do
+    it "adds value to the balance" do
+      subject.deposit(500)
+      expect(subject.balance).to eq 500
     end
   end
 
-  it 'should give the date of a transaction' do
-    subject.transaction(20, 'credit', '21/05/2018')
-    expect(subject.transactions[0]).to include date: '21/05/2018'
+  describe '#withdrawal' do
+    it "subtracts value from the balance" do
+      subject.withdrawal(200)
+      expect{ subject.withdrawal(200) }.to change{ subject.balance }.by(-200)
+    end
   end
 
-  it 'should store all the transactions' do
-    subject.transaction(500, 'credit', '15/04/2017')
-    subject.transaction(300, 'debit', '16/04/2017')
-    expect(subject.transactions).to include({ date: '16/04/2017', type: 'debit', value: 300, balance: 200 },
-                                            date: '15/04/2017', type: 'credit', value: 500, balance: 500)
-  end
 
-  it 'should let the customer see all their transactions, last one first' do
-    subject.transaction(500, 'credit', '15/04/2018')
-    subject.transaction(300, 'debit', '16/04/2018')
-    expect(subject.print).to eq([{ date: '16/04/2018', type: 'debit', value: 300, balance: 200 },
-                                 { date: '15/04/2018', type: 'credit', value: 500, balance: 500 }])
-  end
 end
